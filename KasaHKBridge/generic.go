c3ef68c36db32a68d5b6f79bc32c78b21a7904e1
Scot Bontrager <cloudkucooland@gmail.com>
2024-01-13 14:14:08 -0600

    remove the StatusSvc stuff that never worked

--- KasaHKBridge/generic.go
+++ KasaHKBridge/generic.go
@@ -8,7 +8,6 @@ import (
 	"github.com/brutella/hap/accessory"
 	"github.com/brutella/hap/characteristic"
 	"github.com/brutella/hap/log"
-	"github.com/brutella/hap/service"
 
 	"github.com/cloudkucooland/go-kasa"
 )
@@ -17,10 +16,10 @@ import (
 type generic struct {
 	lastUpdate time.Time // last time the device responded (move to Status)
 	*accessory.A
+	RSSI *rssi
 
-	KasaStatus *StatusSvc   // a service for displaying status info
-	ip         net.IP       // would probably be better to use string
-	Sysinfo    kasa.Sysinfo // contents of the last response from the device
+	ip      net.IP       // would probably be better to use string
+	Sysinfo kasa.Sysinfo // contents of the last response from the device
 }
 
 func (g *generic) getA() *accessory.A {
@@ -50,7 +49,8 @@ func (g *generic) configure(k kasa.Sysinfo, ip net.IP) accessory.Info {
 	g.Sysinfo = k
 	g.lastUpdate = time.Now()
 	g.ip = ip
-	g.KasaStatus = NewStatusSvc()
+
+	g.RSSI = NewRSSI()
 
 	info := accessory.Info{
 		Name:         k.Alias,
@@ -59,28 +59,23 @@ func (g *generic) configure(k kasa.Sysinfo, ip net.IP) accessory.Info {
 		Model:        k.Model,
 		Firmware:     k.SWVersion,
 	}
-
 	return info
 }
 
-func id(g *generic) uint64 {
-	var ID uint64
-
-	// convert 12 chars of the deviceId into a uint64 for the ID
+// convert 12 chars of the deviceId into a uint64 for the ID, g.A must exist first, so can't be part of g.configure
+func (g *generic) setID() {
 	mac, err := hex.DecodeString(g.Sysinfo.DeviceID[:12])
 	if err != nil {
 		log.Info.Printf("weird kasa devid: %s", err.Error())
+	} else {
+		var ID uint64
+		for k, v := range mac {
+			ID += uint64(v) << (12 - k) * 8
+		}
+		g.A.Id = ID
 	}
-	for k, v := range mac {
-		ID += uint64(v) << (12 - k) * 8
-	}
-	return ID
-}
 
-func (g *generic) finalize() {
-	// set the ID so the device remains consistent in homekit across reboots
-	g.A.Id = id(g)
-	// g.AddS(g.KasaStatus.S)
+	// g.A.Ss[0].AddC(g.RSSI.C)
 }
 
 func (g *generic) genericUpdate(k kasa.KasaDevice, ip net.IP) {
@@ -96,7 +91,7 @@ func (g *generic) genericUpdate(k kasa.KasaDevice, ip net.IP) {
 	}
 
 	// log.Info.Printf("[%s] RSSI: [%d]", g.Sysinfo.Alias, k.GetSysinfo.Sysinfo.RSSI)
-	g.KasaStatus.RSSI.SetValue(int(k.GetSysinfo.Sysinfo.RSSI))
+	g.RSSI.SetValue(int(k.GetSysinfo.Sysinfo.RSSI))
 	g.lastUpdate = time.Now()
 }
 
@@ -115,29 +110,6 @@ func kpm2hpm(kasaMode string) int {
 		i = characteristic.ProgramModeNoProgramScheduled
 	}
 	return i
-}
-
-type StatusSvc struct {
-	*service.S
-
-	Name *characteristic.Name
-	RSSI *rssi
-}
-
-func NewStatusSvc() *StatusSvc {
-	svc := StatusSvc{}
-	svc.S = service.New("E881")
-	svc.S.Primary = false
-	// svc.S.Hidden = true
-
-	svc.Name = characteristic.NewName()
-	svc.Name.SetValue("Kasa Status")
-	svc.S.AddC(svc.Name.C)
-
-	svc.RSSI = NewRSSI()
-	svc.S.AddC(svc.RSSI.C)
-
-	return &svc
 }
 
 func (g *generic) updateEmeter(e kasa.EmeterRealtime) {
