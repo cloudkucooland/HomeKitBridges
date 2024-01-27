69219346e8f7182155ce4d66dff78551d39a8941
Scot Bontrager <cloudkucooland@gmail.com>
2024-01-27 16:06:28 -0600

    0.0.2...

--- KasaHKBridge/bridge.go
+++ KasaHKBridge/bridge.go
@@ -12,11 +12,11 @@ var root *accessory.Bridge
 // Bridge is used by the startup to build the generic bridge type on which all other devices hang
 func Bridge() *accessory.A {
 	root = accessory.NewBridge(accessory.Info{
-		Name:         "KasaHomekitBridge",
+		Name:         "Kasa-Homekit Bridge",
 		SerialNumber: "1101",
 		Manufacturer: "cloudkucooland",
-		Model:        "KasaHomekitbridge",
-		Firmware:     "0.0.1",
+		Model:        "kasa-homekit",
+		Firmware:     "0.0.2",
 	})
 	root.A.Id = 1
 
