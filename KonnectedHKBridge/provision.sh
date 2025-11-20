b579fa82685c92964700827394f19ccf3ff6411c
Scot Bontrager <cloudkucooland@gmail.com>
2025-11-19 20:47:20 -0600

    bump

--- KonnectedHKBridge/provision.sh
+++ KonnectedHKBridge/provision.sh
@@ -2,4 +2,4 @@
 #
 #{"actuators":[ {"pin":8, "trigger": 1} ]}
 #
-curl --request POST --header "content-type: application/json" --data '{"endpoint_type":"rest","endpoint":"http://192.168.100.5:8999/konnected","token":"notyet", "sensors":[{"pin":1},{"pin":2},{"pin":5},{"pin":6},{"pin":7}] }' http://192.168.100.63:14996/settings
+curl --request POST --header "content-type: application/json" --data '{"endpoint_type":"rest","endpoint":"http://192.168.100.5:8999/konnected","token":"notyet", "sensors":[{"pin":1},{"pin":2},{"pin":5},{"pin":6},{"pin":7}] }' http://192.168.100.2:14996/settings
